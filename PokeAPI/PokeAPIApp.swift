d60063c1398152923142c839be6a0e6265f2bf5a
Cristian Almendro <cristian.almendro@jalasoft.com>
2023-05-16 16:52:33 -0400

    Initial Commit

---
+++ PokeAPI/PokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPIApp.swift
+//  PokeAPI
+//
+//  Created by Cristian Misael Almendro Lazarte on 16/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
